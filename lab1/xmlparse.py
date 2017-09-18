7a8c3dc394f39d5ccbf09d77ae9610518137f4c1
Folofl <alena.borisenko@gmail.com>
2017-09-18 14:56:10 -0400

    extract chapter titles

--- lab1/xmlparse.py
+++ lab1/xmlparse.py
@@ -3,11 +3,28 @@
 # Created:    September 18th, 2017
 # Submitted:  September 20th, 2017
 
+import re
+
 source = open("Blithedale_Romance.txt", "r")
 target = open("Blithedale_Romance.xml", "w")
 
+data = source.read()
 
 target.write("<book>\n")
+
+chaptertitle = r"[I|V|X]+\. [A-Z '-]{2,}"
+
+match = re.findall(chaptertitle, data)
+
+if match:
+    for x in match:
+        target.write("<chaptertitle>")
+        target.write("".join(x))
+        target.write("</chaptertitle>\n")
+
+
+
+
 target.write("\n</book>")
 
 source.close()
