8989bb73728818bcb698a902b38e932039df1fb6
Alena Borisenko <alena.borisenko@gmail.com>
2017-10-12 09:38:14 -0400

    lab 5 posted

---
+++ lab5/Lab5.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Corpus: I like deep learning. I like NLP. I enjoy sailing.
+la = np.linalg
+words = ["I", "like", "enjoy", "deep", "learning", "NLP", "sailing", "."]
+
+X = np.array([[0,2,1,0,0,0,0,0],
+			  [2,0,0,1,0,1,0,0],
+			  [1,0,0,0,0,0,1,0],
+			  [0,1,0,0,1,0,0,0],
+			  [0,0,0,1,0,0,0,1],
+			  [0,1,0,0,0,0,0,1],
+			  [0,0,1,0,0,0,0,1],
+			  [0,0,0,0,1,1,1,0]])
+
+U, s, V = la.svd(X, full_matrices=False)
+
+for i in range(len(words)):
+	plt.text(U[i, 0], U[i, 1], words[i])
+
+plt.show()
