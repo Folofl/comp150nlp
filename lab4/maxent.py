29a16458fd7c7b398ecbe685c5a5bc6465002e6d
Folofl <alena.borisenko@gmail.com>
2017-10-12 23:03:14 -0400

    can extract necessary xml info

--- lab4/maxent.py
+++ lab4/maxent.py
@@ -3,13 +3,33 @@
 # Submitted:  October 13th, 2017
 #
 # Note:       Using Noah Cooper's updated xml file
+#             Not sure about MacOS, but Windows gives a unicode
+#             error unless you run the following in the console:
+#                 chcp 65001
 
 import nltk
 import sys
+import xml.etree.ElementTree as ET
 
 # read in command line arguments specifying filenames
 train_file_name = sys.argv[-2]
-test_file_name  = sys.argv[-1] 
+test_file_name  = sys.argv[-1]
+
+tree = ET.parse(train_file_name)
+reviews = tree.getroot()
+
+for review in reviews:
+    asin        = review.find('asin').text
+    rating      = review.find('rating').text
+    helpful     = review.find('helpful').text
+    review_text = review.find('review_text').text
+    print(asin)
+    print(rating)
+    print(helpful)
+    print(review_text)
+
+# features
+
 
 # output:
 # uniqueID $tab$ Positive/Negative
