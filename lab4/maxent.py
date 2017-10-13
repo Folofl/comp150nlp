e7922cb653195b8e0cc1dde3e5cbdbb63d25590f
Folofl <alena.borisenko@gmail.com>
2017-10-13 00:10:37 -0400

    get 2000 most common words from ALL reviews

--- lab4/maxent.py
+++ lab4/maxent.py
@@ -15,18 +15,53 @@ import xml.etree.ElementTree as ET
 train_file_name = sys.argv[-2]
 test_file_name  = sys.argv[-1]
 
+# gain access to XML data tags 
 tree = ET.parse(train_file_name)
 reviews = tree.getroot()
 
+# gather ALL the possible words from reviews AND titles
+all_words = []
+
 for review in reviews:
-    asin        = review.find('asin').text
-    rating      = review.find('rating').text
-    helpful     = review.find('helpful').text
-    review_text = review.find('review_text').text
-    print(asin)
-    print(rating)
-    print(helpful)
-    print(review_text)
+    title       = review.find('title').text.strip().lower().split()
+    review_text = review.find('review_text').text.strip().lower().split()
+    all_words.extend(title)
+    all_words.extend(review_text)
+
+all_words = nltk.FreqDist(all_words)
+all_words = all_words.most_common(2000)
+word_features = list(i[0] for i in all_words)
+print(word_features)
+
+
+
+# features
+# def review_features(review):
+#     features = {}
+#     for word in review:
+#         features['contains({})'.format(word)] = word in re
+
+# for review in reviews:
+#     asin        = review.find('asin').text
+#     print(asin)
+#     rating      = review.find('rating').text.strip()
+#     print(rating)
+#     if float(rating) >= 4:
+#         rating = 'Positive'
+#     else:
+#         rating = 'Negative'
+#     helpful     = review.find('helpful').text.split()
+#     if len(helpful) == 3:
+#         helpful = int(helpful[0]) / int(helpful[2])
+#     else:
+#         helpful = -1
+#     review_text = review.find('review_text').text
+    
+#     print(rating)
+#     print(helpful)
+#     print(review_text)
+#     print('hallelujah' in review_text)
+
 
 # features
 
