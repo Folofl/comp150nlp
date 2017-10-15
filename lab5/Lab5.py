2af6b4e4263bc3f9935930caa2f804e05e0bc6ce
Folofl <alena.borisenko@gmail.com>
2017-10-15 01:48:12 -0400

    generate and write term-term matrix

--- lab5/Lab5.py
+++ lab5/Lab5.py
@@ -16,14 +16,19 @@ except LookupError:
     nltk.download('brown')
 
 
+#file_matrix = open("matrix.txt", "w")
+
+np.set_printoptions(threshold=np.nan)
+np.set_printoptions(suppress=True)
+
 la = np.linalg
 
 # corpus contains all words as they appear in brown corpus
 #corpus = brown.words()
-#corpus = brown.words(categories='romance')
-corpus = ["a", "b", "c", "a", "c", "d", "e", "a", "c", "d"]
+corpus = brown.words(categories='romance')
+#corpus = ["I", "like", "deep", "learning", ".", "I", "like", "NLP", ".", "I", "enjoy", "sailing", "."]
+#corpus = ["a", "b", "c", "a", "c", "d", "e", "a", "c", "d"]
 corp_len = len(corpus)
-print(corp_len)
 
 # words contains mapping: UNIQUE word key -> index in the X matrix
 index = 0
@@ -33,23 +38,32 @@ for i in corpus:
         words[i] = index;
         index += 1
 
+# words = {"I":0, "like":1, "enjoy":2, "deep":3, "learning":4, "NLP":5, "sailing":6, ".":7}
 dim = len(words)
-print(dim)
 
 # create a 2D array of size dim x dim; set all values to 0
-X = np.zeros(shape=(dim, dim))
+X = np.zeros(shape=(dim, dim), dtype=int)
 
-# win_size = 2
-# for i in range(0, corp_len):
-#     if i < win_size:
-        
-#     elif i > (corp_len - win_size):
-#     else:
+win_size = 7
+for i in range(0, corp_len):
+    # check the left side
+    left_i = i - 1
+    countdown = win_size
+    while (left_i >= 0) and (countdown > 0):
+        X[ words[corpus[i]] ] [ words[corpus[left_i]] ] += 1
+        left_i    -= 1
+        countdown -= 1
+    # check the right side
+    right_i = i + 1
+    countdown = win_size
+    while (right_i < corp_len) and (countdown > 0):
+        X[ words[corpus[i]] ] [ words[corpus[right_i]] ] += 1
+        right_i   += 1
+        countdown -= 1
 
+# save the matrix to a file for later use
+np.savetxt('matrix.txt', X, fmt='%i')
 
-
-
-# print(X)
 
 
 #Corpus: I like deep learning. I like NLP. I enjoy sailing.
@@ -57,13 +71,13 @@ X = np.zeros(shape=(dim, dim))
 # words = ["I", "like", "enjoy", "deep", "learning", "NLP", "sailing", "."]
 
 # X2 = np.array([[0,2,1,0,0,0,0,0],
-# 			  [2,0,0,1,0,1,0,0],
-# 			  [1,0,0,0,0,0,1,0],
-# 			  [0,1,0,0,1,0,0,0],
-# 			  [0,0,0,1,0,0,0,1],
-# 			  [0,1,0,0,0,0,0,1],
-# 			  [0,0,1,0,0,0,0,1],
-# 			  [0,0,0,0,1,1,1,0]])
+# 			     [2,0,0,1,0,1,0,0],
+# 			     [1,0,0,0,0,0,1,0],
+# 			     [0,1,0,0,1,0,0,0],
+# 			     [0,0,0,1,0,0,0,1],
+# 			     [0,1,0,0,0,0,0,1],
+# 			     [0,0,1,0,0,0,0,1],
+# 			     [0,0,0,0,1,1,1,0]])
 # print(X2)
 
 # U, s, V = la.svd(X, full_matrices=False)
