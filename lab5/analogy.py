7599b41b61b65d5ecae4d4629344257c15ca5bdb
Folofl <alena.borisenko@gmail.com>
2017-10-18 16:26:40 -0400

    basic (and really inaccurate...) analogies

--- lab5/analogy.py
+++ lab5/analogy.py
@@ -15,7 +15,7 @@ with open ('keys', 'rb') as fp:
 # unpickle matrix word dict
 with open ('words', 'rb') as fp:
     words = pickle.load(fp)
-    
+
 # load the X matrix
 X = np.load('Xmatrix.npy')
 # load the U matrix
@@ -36,9 +36,31 @@ def find_analogy(A, B, C):
     if D == "ERROR":
         return D
 
-    print(words[A])
-    print(words[B])
-    print(words[C])
+    ABdiff  = np.subtract(X[ words[A] ], X[ words[B] ])
+    ABCdiff = np.subtract(ABdiff, X[ words[C] ])
+    print(ABdiff)
+    print(ABCdiff)
+
+    min_sum = 10000000
+    for key in keys:
+        if key != A and key != B and key != C:
+            ABCD    = np.add(ABCdiff, X[ words[key] ])
+            ABCD2   = np.power(ABCD, 2)
+            ABCDsum = np.sum(ABCD2)
+
+            if ABCDsum < min_sum:
+                min_sum = ABCDsum
+                D = key
+
+    print(min_sum)
+
+
+    # print(U[ words[A] ])
+    # print(U[ words[B] ])
+    # print(U[ words[C] ])
+
+    # for key in keys:
+
 
     return D
 
