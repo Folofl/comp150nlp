d2deccbf827fceddc9ebb30362b5c193519059f4
Folofl <alena.borisenko@gmail.com>
2017-10-17 23:29:01 -0400

    removed stop words and punctuation

--- lab5/Lab5.py
+++ lab5/Lab5.py
@@ -4,16 +4,10 @@
 # Submitted:  October 18th, 2017
 
 import nltk
-from   nltk.corpus   import brown
-from   nltk.corpus   import stopwords
-from   nltk.tokenize import RegexpTokenizer
+from nltk.corpus import brown
 
-import time
-import re
 import numpy as np
 import matplotlib.pyplot as plt
-
-from operator import itemgetter
 
 # check if package is installed 
 try:
@@ -21,49 +15,34 @@ try:
 except LookupError:
     nltk.download('brown')
 
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
-
 np.set_printoptions(threshold=np.nan)
 np.set_printoptions(suppress=True)
 
 la = np.linalg
 
 # corpus contains all words as they appear in brown corpus
-#corpus = brown.raw()
-corpus = brown.raw(categories='romance')
-#corpus = "I think that is a cat, what about the dog's? Don't know"
-
-corpus = corpus.lower()
-
-# remove punctuation
-tokenizer = RegexpTokenizer(r'\w+')
-corpus = tokenizer.tokenize(corpus)
-
-# remove stopwords
-stopwords = set(stopwords.words('english'))
-corpus = [word for word in corpus if word not in stopwords]
-
+#corpus = brown.words()
+#corpus = brown.words(categories='romance')
+#corpus = ["I", "like", "deep", "learning", ".", "I", "like", "NLP", ".", "I", "enjoy", "sailing", "."]
+corpus = ["a", "b", "c", "a", "c", "d", "e", "a", "c", "d"]
 corp_len = len(corpus)
 
 # words contains mapping: UNIQUE word key -> index in the X matrix
 index = 0
-words = {}
+words   = {}
+indices = {}
 for i in corpus:
     if i not in words:
         words[i] = index;
         index += 1
+print(words)
 
-sorted_words = sorted(words.items(), key=itemgetter(1))
-
+# words = {"I":0, "like":1, "enjoy":2, "deep":3, "learning":4, "NLP":5, "sailing":6, ".":7}
 dim = len(words)
 
-#create a 2D array of size dim x dim; set all values to 0
+# create a 2D array of size dim x dim; set all values to 0
 X = np.zeros(shape=(dim, dim), dtype=int)
 
-start = time.time()
 win_size = 7
 for i in range(0, corp_len):
     # check the left side
@@ -81,12 +60,9 @@ for i in range(0, corp_len):
         right_i   += 1
         countdown -= 1
 
-print("Made X in %s seconds" % (time.time() - start))
+# save the matrix to a file for later use
+#np.savetxt('matrix.txt', X, fmt='%i')
 
-# start = time.time()
-# # save the matrix to a file for later use
-# np.savetxt('matrix.txt', X, fmt='%i')
-# print("---- %s seconds ---" % (time.time() - start))
 
 
 #Corpus: I like deep learning. I like NLP. I enjoy sailing.
